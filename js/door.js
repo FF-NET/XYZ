b49045899e44cdbc164722c8d6de99d767c3659f
FF-NET <166034446+FF-NET@users.noreply.github.com>
2024-04-05 00:45:50 +0900

    external js

---
+++ js/door.js
@@ -0,0 +1,8 @@
+<script>
+var pages = ["༺═──────═༻", "━━━☞", "-̶̶•-̶̶•̸Ϟ•̸•••̸Ϟ•̸-̶̶•-̶"];
+
+function eཧ() {
+    var randomIndex = Math.floor(Math.random() * pages.length);
+    window.open(pages[randomIndex], '_blank');
+}
+</script>
