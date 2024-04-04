0701532a2276b87c6fe9dba87eb80480c19174c3
FF-NET <166034446+FF-NET@users.noreply.github.com>
2024-04-05 00:56:58 +0900

    /\___/\ (=✪o✪=)

--- js/door.js
+++ js/door.js
@@ -1,8 +1,1 @@
-<script>
-var pages = ["༺═──────═༻", "━━━☞", "-̶̶•-̶̶•̸Ϟ•̸•••̸Ϟ•̸-̶̶•-̶"];
-
-function eཧ() {
-    var randomIndex = Math.floor(Math.random() * pages.length);
-    window.open(pages[randomIndex], '_blank');
-}
-</script>
+var pages=["༺═──────═༻","━━━☞","-̶̶•-̶̶•̸Ϟ•̸•••̸Ϟ•̸-̶̶•-̶"];function eཧ(){var a=Math.floor(Math.random()*pages.length);window.open(pages[a],"_blank")}
