c39dfe63af028e24f009166dbcb08592cc0ab851
FF-NET <166034446+FF-NET@users.noreply.github.com>
2024-06-29 17:25:23 +0900

    Update RandomMapTransfer.js

--- 240629/js/plugins/RandomMapTransfer.js
+++ 240629/js/plugins/RandomMapTransfer.js
@@ -20,7 +20,7 @@
     'use strict';
 
     // 제외할 맵 ID를 정의합니다. 예를 들어, 맵 ID 9를 제외합니다.
-    const excludedMapIds = [9, 19, 20, 21,];
+    const excludedMapIds = [9, 19, 20, 21];
 
     // RPG Maker MZ의 데이터 파일에서 모든 맵 ID를 가져옵니다.
     function getAllMapIds() {
@@ -57,17 +57,13 @@
         goToRandomMap(); // 랜덤 맵으로 이동하는 함수를 호출합니다.
     });
 
-    // Scene_Map 클래스의 start 메서드를 오버라이드하여 맵 로드 시 페이드 인을 추가합니다.
-    const _Scene_Map_start = Scene_Map.prototype.start;
-    Scene_Map.prototype.start = function() {
-        _Scene_Map_start.call(this);
-        
+    // Scene_Map 클래스의 onMapLoaded 메서드를 오버라이드하여 맵 로드 완료 후 페이드 인을 추가합니다.
+    const _Scene_Map_onMapLoaded = Scene_Map.prototype.onMapLoaded;
+    Scene_Map.prototype.onMapLoaded = function() {
+        _Scene_Map_onMapLoaded.call(this);
+
         const fadeInDuration = 60; // 페이드 인 시간 (프레임 단위)
-        if ($gamePlayer.isTransferring()) {
-            // 만약 플레이어가 전송 중이라면, 페이드 인을 수행하지 않습니다.
-            return;
-        }
-        // 맵 로드 시 페이드 인을 시작합니다.
+        // 맵 로드가 완료된 후 페이드 인을 시작합니다.
         $gameScreen.startFadeIn(fadeInDuration);
     };
 
